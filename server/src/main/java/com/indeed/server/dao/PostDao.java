a08b3951a18d345a9c7d62f04772a63a68d0e91b
aniket <aniketmeshram275@gmail.com>
2026-02-24 03:02:04 +0530

    update

--- server/src/main/java/com/indeed/server/dao/PostDao.java
+++ server/src/main/java/com/indeed/server/dao/PostDao.java
@@ -2,9 +2,10 @@ package com.indeed.server.dao;
 
 import com.indeed.server.model.PostModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface PostDao extends MongoRepository<PostModel, String> {
 
 }
