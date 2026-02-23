2aed5d86e5e0d29a62fdb60ed507204847396886
aniket <aniketmeshram275@gmail.com>
2026-02-23 17:15:25 +0530

    updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const API_URL = "https://indeed-application.onrender.com";
+
 
 
 export const savePost = async (payload) => {
